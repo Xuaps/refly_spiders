41f6ca1dc7bb8d4fa72896f02e67ba8b41a3e56b
David Vilchez <dvilchez@xuaps.com>
2014-12-08 11:46:09 +0100

    organized spider module

---
+++ js.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import urlparse
+import re
+from refly_scraper.items import ReferenceItem
+
+
+class JsSpider(scrapy.Spider):
+    name = 'JavaScript'
+    excluded_path = ['MDN', 'Web technology for developers']
+    allowed_domains = ['mozilla.org']
+    visited = []
+    start_urls = (
+        'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference',
+    )
+
+    def parse(self, response):
+
+        reference = ReferenceItem()
+        reference['name'] = response.xpath('//h1/text()').extract()[0]
+        reference['url'] = urlparse.urlsplit(response.url)[2]
+        reference['content'] = response.xpath('//article').extract()[0]
+        reference['path'] = [p for p in response.css('.crumbs').xpath('.//a/text()').extract() if p not in self.excluded_path]
+
+        yield reference
+
+        urls = [self.visit(urlparse.urljoin(response.url, url)) for url in response.xpath('//a[re:test(@href, "^\/en-US\/docs\/Web\/JavaScript\/Reference((?!\\$|#).)*$")]/@href').extract() if url not in self.visited]
+        for i in urls:
+            yield i
+
+    def visit(self, url):
+        self.visited.append(url)
+        return scrapy.Request(url, callback=self.parse)
+
+    def resolveType(self, url, name):
+        if re.search(r'^.*Statements\/((?!\/).)*$',url)!=None:
+            return 'statement'
+        elif re.search(r'^.*Operators\/((?!\/).)*$',url)!=None:
+            return 'expression'
+        elif re.search(r'^.*Global_Objects\/.*(?=\/).*$',url)!=None and re.search(r'^.*(?=\)$)',name)!=None:
+            return 'method'
+        elif re.search(r'^.*Global_Objects\/.*(?=\/).*$',url)!=None and re.search(r'^((?!\().)*$',name)!=None:
+            return 'property'
+        elif re.search(r'^.*Global_Objects\/((?!\/).)*$',url)!=None and re.search(r'^[A-Z]((?!\().)*$',name)!=None:
+            return 'class'
+        elif re.search(r'^.*Global_Objects\/((?!\/).)*$',url)!=None and re.search(r'^.*(?=\)$)',name)!=None:
+            return 'function'
+        elif re.search(r'^.*Global_Objects\/((?!\/).)*$',url)!=None and re.search(r'^[a-z]((?!\().)*$', name)!=None:
+            return 'object'
+    
+        return "others";
+
+    def getSlashUrl(self,path, name):
+        return '/'+('/'.join(path)+'/'+name).lower()
---
+++ tests/test_javascript_parser.py
@@ -0,0 +1,27 @@
+import unittest
+from scrapy.http import HtmlResponse
+from refly_scraper.items import ReferenceItem
+from refly_spiders.js import JsSpider
+
+    
+class JavaScriptParserTest(unittest.TestCase):
+    def test_parse(self):
+        response = HtmlResponse(url='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference', 
+        	body=open('./tests/data/javascript_reference.html').read())
+        spider = JsSpider()
+
+        result = spider.parse(response)
+        item = result.next()
+
+        self.assertEqual(item['name'], 'JavaScript reference')
+        self.assertEqual(item['url'], '/en-US/docs/Web/JavaScript/Reference')
+        self.assertIsNotNone(item['content'])
+        self.assertEqual(item['path'], ['JavaScript'])
+
+    def test_getSlashUrl(self):
+        pass
+
+    def test_resolveType(self):
+        pass
+
+        
